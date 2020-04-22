71a57adc2b5d643d41b791613b47a3b1fb46a0e4
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-22 19:00:09 -0300

    Entity Framework: integrando a camada de acesso a dados ao repositório

---
+++ TreinaWeb.CadastroPessoas.Repositorio/PessoaRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using CadastroPessoas.Dominio;
+using CadastroPessoas.Persistencia.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinaWeb.CadastroPessoas.Repositorio
+{
+    public class PessoaRepositorio : IRepositorio<Pessoa>
+    {
+
+        public List<Pessoa> SelecionarTodos()
+        {
+            CadastroPessoasDbContext contexto = new CadastroPessoasDbContext();
+            List<Pessoa> pessoas = contexto.Pessoas.OrderBy(x => x.Nome).ToList();
+            contexto.Dispose();
+            return pessoas;
+        }
+
+        public int Adicionar(Pessoa objeto)
+        {
+            CadastroPessoasDbContext contexto = new CadastroPessoasDbContext();
+            contexto.Pessoas.Add(objeto);
+            return contexto.SaveChanges();
+        }
+
+    }
+}
