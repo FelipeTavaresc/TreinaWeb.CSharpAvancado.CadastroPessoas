85c19b39f3935c091f22b04e3db391f143b10304
Felipe Tavares <felipe.tavares@outlook.com>
2020-04-27 20:22:45 -0300

    Entity Framework: fazendo nossa camada de apresentação conversar com nossos repositórios e com o Entity Framework

---
+++ TreinaWeb.CSharpAvancado.CadastroPessoas/FrmPessoas.cs
@@ -0,0 +1,42 @@
+﻿using CadastroPessoas.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TreinaWeb.CadastroPessoas.Repositorio;
+
+namespace TreinaWeb.CSharpAvancado.CadastroPessoas
+{
+    public partial class FrmPessoas : Form
+    {
+        public FrmPessoas()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PreencherDataGridView();
+        }
+
+        private void PreencherDataGridView()
+        {
+            IRepositorio<Pessoa> repositorioPessoa = new PessoaRepositorio();
+            List<Pessoa> pessoas = repositorioPessoa.SelecionarTodos();
+            dgvPessoas.DataSource = pessoas;
+            dgvPessoas.Refresh();
+        }
+
+        private void btnAdicionarPessoa_Click(object sender, EventArgs e)
+        {
+            FrmAdicionarPessoa frmAdicionarPessoa = new FrmAdicionarPessoa();
+            frmAdicionarPessoa.ShowDialog();
+            PreencherDataGridView();
+        }
+    }
+}
