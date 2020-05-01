115bb158705abb1ea93b31ec0d4a63a2a269bffc
Felipe Tavares <felipe.tavares@outlook.com>
2020-05-01 18:38:07 -0300

    NHibernate: integrando-o ao repositório e à camada de apresentação

--- TreinaWeb.CadastroPessoas.Repositorio/PessoaRepositorio.cs
+++ TreinaWeb.CadastroPessoas.Repositorio/PessoaRepositorio.cs
@@ -15,24 +15,24 @@ namespace TreinaWeb.CadastroPessoas.Repositorio
 {
     public class PessoaRepositorio : IRepositorio<Pessoa>
     {
-        private ISessionFactory _sessionFactory;
+        //private ISessionFactory _sessionFactory;
 
-        public PessoaRepositorio()
-        {
-            Configuration config = new Configuration();
-            config.Configure();
-            config.AddAssembly(typeof(Pessoa).Assembly);
-            HbmMapping mapping = CreateMappings();
-            config.AddDeserializedMapping(mapping, null);
-            _sessionFactory = config.BuildSessionFactory();
-        }
+        //public PessoaRepositorio()
+        //{
+        //    Configuration config = new Configuration();
+        //    config.Configure();
+        //    config.AddAssembly(typeof(Pessoa).Assembly);
+        //    HbmMapping mapping = CreateMappings();
+        //    config.AddDeserializedMapping(mapping, null);
+        //    _sessionFactory = config.BuildSessionFactory();
+        //}
 
-        private HbmMapping CreateMappings()
-        {
-            ModelMapper mapper = new ModelMapper();
-            mapper.AddMapping(typeof(PessoaMap));
-            return mapper.CompileMappingForAllExplicitlyAddedEntities();
-        }
+        //private HbmMapping CreateMappings()
+        //{
+        //    ModelMapper mapper = new ModelMapper();
+        //    mapper.AddMapping(typeof(PessoaMap));
+        //    return mapper.CompileMappingForAllExplicitlyAddedEntities();
+        //}
 
         public List<Pessoa> SelecionarTodos()
         {
@@ -40,6 +40,13 @@ namespace TreinaWeb.CadastroPessoas.Repositorio
             List<Pessoa> pessoas = contexto.Pessoas.OrderBy(x => x.Nome).ToList();
             contexto.Dispose();
             return pessoas;
+
+            /*
+            using (ISession sessao = _sessionFactory.OpenSession())
+            {
+                IQuery consulta = sessao.CreateQuery("FROM Pessoa");
+                return consulta.List<Pessoa>().ToList();
+            }*/
         }
 
         public int Adicionar(Pessoa objeto)
@@ -47,6 +54,18 @@ namespace TreinaWeb.CadastroPessoas.Repositorio
             CadastroPessoasDbContext contexto = new CadastroPessoasDbContext();
             contexto.Pessoas.Add(objeto);
             return contexto.SaveChanges();
+
+            /*
+            using (ISession sessao = _sessionFactory.OpenSession())
+            {
+                using (var trasacao = sessao.BeginTransaction())
+                {
+                    sessao.Save(objeto);
+                    trasacao.Commit();
+                    return 0;
+                }
+            }
+            */
         }
 
     }
