81634435adb4facd2944a356c42b55260e3f54a6
Felipe Tavares <felipe.tavares@outlook.com>
2020-05-11 20:42:37 -0300

    Tasks: criando uma task para preencher um datagridview

--- TreinaWeb.CSharpAvancado.CadastroPessoas/FrmPessoas.cs
+++ TreinaWeb.CSharpAvancado.CadastroPessoas/FrmPessoas.cs
@@ -16,6 +16,7 @@ namespace TreinaWeb.CSharpAvancado.CadastroPessoas
     public partial class FrmPessoas : Form
     {
         private List<Pessoa> _pessoas = new List<Pessoa>();
+        private static readonly object locker = new object();
 
         public FrmPessoas()
         {
@@ -24,30 +25,80 @@ namespace TreinaWeb.CSharpAvancado.CadastroPessoas
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Thread thread = new Thread(PreencherDataGridView);
-            thread.Start();
+            //Thread thread = new Thread(PreencherDataGridView);
+            //thread.Start();
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                IRepositorio<Pessoa> repositorioPessoa = new PessoaRepositorio();
+                dgvPessoas.Invoke((MethodInvoker)delegate {
+                    dgvPessoas.DataSource = repositorioPessoa.SelecionarTodos();
+                    dgvPessoas.Refresh();
+                });
+            });
         }
 
         private void PreencherDataGridView()
         {
             Thread.Sleep(5000);
             Thread thread = new Thread(PreencherListaPessoas);
+            Thread thread2 = new Thread(PreencherListaPessoas2);
             thread.Start();
+            thread2.Start();
             thread.Join();
+            thread2.Join();
             dgvPessoas.Invoke((MethodInvoker)delegate { dgvPessoas.DataSource = _pessoas; dgvPessoas.Refresh(); });
         }
 
         private void PreencherListaPessoas()
         {
             IRepositorio<Pessoa> repositorioPessoa = new PessoaRepositorio();
-            _pessoas = repositorioPessoa.SelecionarTodos();
+            List<Pessoa> pessoas = repositorioPessoa.SelecionarTodos();
+            lock (locker)
+            {
+                foreach (Pessoa p in pessoas)
+                {
+                    p.Nome += " Thread 1";
+                    _pessoas.Add(p);
+                    Thread.Sleep(300);
+                }
+            }
+        }
+
+        private void PreencherListaPessoas2()
+        {
+            try
+            {
+                throw new Exception("Teste");
+                IRepositorio<Pessoa> repositorioPessoa = new PessoaRepositorio();
+                List<Pessoa> pessoas = repositorioPessoa.SelecionarTodos();
+                lock (locker)
+                {
+                    foreach (Pessoa p in pessoas)
+                    {
+                        p.Nome += " Thread 2";
+                        _pessoas.Add(p);
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ExibirErro(ex);
+            }
+
+        }
+
+        private void ExibirErro(Exception ex)
+        {
+            MessageBox.Show(ex.Message);
         }
 
         private void btnAdicionarPessoa_Click(object sender, EventArgs e)
         {
             FrmAdicionarPessoa frmAdicionarPessoa = new FrmAdicionarPessoa();
             frmAdicionarPessoa.ShowDialog();
-            PreencherDataGridView();
+            //PreencherDataGridView();
         }
     }
 }
